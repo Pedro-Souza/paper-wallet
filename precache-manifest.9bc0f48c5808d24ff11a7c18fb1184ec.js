10c7c0857eaa2d1b7ff3a4dfceb7e57b945b715c
ItsANameToo <itsanametoo@protonmail.com>
2019-09-04 10:56:38 +0200

    deploy [ci skip]

---
+++ precache-manifest.9bc0f48c5808d24ff11a7c18fb1184ec.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8cae31e8b947027e6c4",
+    "url": "/paper-wallet/css/app.332dcb79.css"
+  },
+  {
+    "revision": "d8cae31e8b947027e6c4",
+    "url": "/paper-wallet/js/app.cd9123c6.js"
+  },
+  {
+    "revision": "62bdcd5796c2fce32476",
+    "url": "/paper-wallet/css/chunk-vendors.84c8a1ab.css"
+  },
+  {
+    "revision": "62bdcd5796c2fce32476",
+    "url": "/paper-wallet/js/chunk-vendors.77378965.js"
+  },
+  {
+    "revision": "89845e83a2bc94b41b77",
+    "url": "/paper-wallet/css/message.919d5c1a.css"
+  },
+  {
+    "revision": "89845e83a2bc94b41b77",
+    "url": "/paper-wallet/js/message.b4a356ee.js"
+  },
+  {
+    "revision": "6aa188bf579ca2fa7039",
+    "url": "/paper-wallet/css/message:sign.9d3bc10c.css"
+  },
+  {
+    "revision": "6aa188bf579ca2fa7039",
+    "url": "/paper-wallet/js/message:sign.d4c9f184.js"
+  },
+  {
+    "revision": "415897d00d61cbe7ce35",
+    "url": "/paper-wallet/js/message:sign~message:verify~wallet:entropy~wallet:passphrase.6fb68009.js"
+  },
+  {
+    "revision": "cb4c720387c30b660294",
+    "url": "/paper-wallet/js/message:sign~wallet:entropy~wallet:passphrase.d35a83de.js"
+  },
+  {
+    "revision": "a6b0f6d650deaf45886c",
+    "url": "/paper-wallet/css/message:verify.a6aee66b.css"
+  },
+  {
+    "revision": "a6b0f6d650deaf45886c",
+    "url": "/paper-wallet/js/message:verify.91b448e1.js"
+  },
+  {
+    "revision": "f615cea02b6fbe35bf08",
+    "url": "/paper-wallet/css/wallet.7757e91a.css"
+  },
+  {
+    "revision": "f615cea02b6fbe35bf08",
+    "url": "/paper-wallet/js/wallet.74889368.js"
+  },
+  {
+    "revision": "57ff42ce33e1c965ae90",
+    "url": "/paper-wallet/css/wallet:entropy.c9fd925b.css"
+  },
+  {
+    "revision": "57ff42ce33e1c965ae90",
+    "url": "/paper-wallet/js/wallet:entropy.646209ab.js"
+  },
+  {
+    "revision": "451eca31804d733af99f",
+    "url": "/paper-wallet/css/wallet:passphrase.a6aee66b.css"
+  },
+  {
+    "revision": "451eca31804d733af99f",
+    "url": "/paper-wallet/js/wallet:passphrase.797b98f4.js"
+  },
+  {
+    "revision": "a8db2e3cdcb207d66a136af0330fc6b2",
+    "url": "/paper-wallet/img/background.a8db2e3c.png"
+  },
+  {
+    "revision": "3d19f3a7040f8445988b3627c2466341",
+    "url": "/paper-wallet/img/globe.3d19f3a7.png"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/paper-wallet/fonts/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/paper-wallet/fonts/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/paper-wallet/fonts/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "771808a61f870d86f04a8c93c299825b",
+    "url": "/paper-wallet/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/paper-wallet/robots.txt"
+  }
+];
